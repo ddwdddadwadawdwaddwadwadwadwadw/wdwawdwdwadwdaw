0d8e529ab5d8fb13c9045a50cf95af9779eba16c
ehehehehe <skincsint@mailfence.com>
2024-11-25 20:37:17 -0200

    Update script.js

--- kl/script.js
+++ kl/script.js
@@ -1,23 +1,13 @@
-const cursor = document.createElement('div');
-cursor.classList.add('cursor');
-document.body.appendChild(cursor);
-
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = `${e.pageX}px`;
-    cursor.style.top = `${e.pageY}px`;
-});
-
-document.querySelectorAll('.pfp-box, .profile-pic, .social-icons a').forEach((element) => {
-    element.addEventListener('mouseover', () => cursor.classList.add('cursor-hover'));
-    element.addEventListener('mouseout', () => cursor.classList.remove('cursor-hover'));
-});
 
 const audio = document.getElementById('background-audio');
 let isAudioPlayed = false;
 
+
 document.addEventListener('click', () => {
     if (!isAudioPlayed) {
-        audio.play();
+        audio.play().catch(error => {
+            console.error('Audio playback failed:', error);
+        });
         isAudioPlayed = true;
         alert('Audio started playing. Refresh to play again.');
     } else {
